b12300a66e0a9add333f90904b7f12d3f51d3293
Bradley Grainger <bgrainger@gmail.com>
2010-02-01 21:42:19 -0800

    Changed spaces to tabs.

--- tests/Logos.Utility.Tests/ObjectUtilityTests.cs
+++ tests/Logos.Utility.Tests/ObjectUtilityTests.cs
@@ -4,15 +4,15 @@ using NUnit.Framework;
 
 namespace Logos.Utility.Tests
 {
-    [TestFixture]
-    public class ObjectUtilityTests
-    {
-        [TestCase(null, 0)]
-        [TestCase(1, 1)]
-        [TestCase(2, 2)]
-        public void GetHashCode(object obj, int hashCode)
-        {
-            Assert.That(ObjectUtility.GetHashCode(obj), Is.EqualTo(hashCode));
-        }
-    }
+	[TestFixture]
+	public class ObjectUtilityTests
+	{
+		[TestCase(null, 0)]
+		[TestCase(1, 1)]
+		[TestCase(2, 2)]
+		public void GetHashCode(object obj, int hashCode)
+		{
+			Assert.That(ObjectUtility.GetHashCode(obj), Is.EqualTo(hashCode));
+		}
+	}
 }
--- tests/Logos.Utility.Tests/ScopeTests.cs
+++ tests/Logos.Utility.Tests/ScopeTests.cs
@@ -3,56 +3,56 @@ using NUnit.Framework;
 
 namespace Logos.Utility.Tests
 {
-    [TestFixture]
-    public class ScopeTests
-    {
-        [Test]
-        public void Empty()
-        {
-            // should compile and run without errors
-            using (Scope.Empty)
-            {
-            }
-        }
+	[TestFixture]
+	public class ScopeTests
+	{
+		[Test]
+		public void Empty()
+		{
+			// should compile and run without errors
+			using (Scope.Empty)
+			{
+			}
+		}
 
-        [Test]
-        public void Create()
-        {
-            int i = 0;
-            using (Scope.Create(() => i++))
-            {
-                Assert.That(i, Is.EqualTo(0));
-            }
-            Assert.That(i, Is.EqualTo(1));
-        }
+		[Test]
+		public void Create()
+		{
+			int i = 0;
+			using (Scope.Create(() => i++))
+			{
+				Assert.That(i, Is.EqualTo(0));
+			}
+			Assert.That(i, Is.EqualTo(1));
+		}
 
-        [Test]
-        public void Cancel()
-        {
-            int i = 0;
-            using (Scope scope = Scope.Create(() => i++))
-            {
-                Assert.That(i, Is.EqualTo(0));
-                scope.Cancel();
-            }
-            Assert.That(i, Is.EqualTo(0));
-        }
+		[Test]
+		public void Cancel()
+		{
+			int i = 0;
+			using (Scope scope = Scope.Create(() => i++))
+			{
+				Assert.That(i, Is.EqualTo(0));
+				scope.Cancel();
+			}
+			Assert.That(i, Is.EqualTo(0));
+		}
 
-        [Test]
-        public void Transfer()
-        {
-            int i = 0;
-            using (Scope scope = Scope.Create(() => i++))
-            {
-                Assert.That(i, Is.EqualTo(0));
+		[Test]
+		public void Transfer()
+		{
+			int i = 0;
+			using (Scope scope = Scope.Create(() => i++))
+			{
+				Assert.That(i, Is.EqualTo(0));
 
-                using (Scope transferred = scope.Transfer())
-                {
-                    Assert.That(i, Is.EqualTo(0));
-                }
-                Assert.That(i, Is.EqualTo(1));
-            }
-            Assert.That(i, Is.EqualTo(1));
-        }
-    }
+				using (Scope transferred = scope.Transfer())
+				{
+					Assert.That(i, Is.EqualTo(0));
+				}
+				Assert.That(i, Is.EqualTo(1));
+			}
+			Assert.That(i, Is.EqualTo(1));
+		}
+	}
 }
