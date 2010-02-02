95cd1ec808b8b3e92d1af4b6099371dc788bf2e6
Bradley Grainger <bgrainger@gmail.com>
2010-02-01 21:41:22 -0800

    Added Scope and tests.
    Code derived from blog post at http://code.logos.com/blog/2008/08/leverage_using_blocks_with_scope.html.

---
+++ src/Logos.Utility/Scope.cs
@@ -0,0 +1,79 @@
+
+using System;
+
+namespace Logos.Utility
+{
+	/// <summary>
+	/// Executes the specified delegate when disposed.
+	/// </summary>
+    /// <remarks>See <a href="http://code.logos.com/blog/2008/08/leverage_using_blocks_with_scope.html">Leverage using blocks with Scope</a>.</remarks>
+	public sealed class Scope : IDisposable
+	{
+		/// <summary>
+		/// Creates a <see cref="Scope" /> for the specified delegate.
+		/// </summary>
+		/// <param name="fnDispose">The delegate.</param>
+		/// <returns>An instance of <see cref="Scope" /> that calls the delegate when disposed.</returns>
+		/// <remarks>If fnDispose is null, the instance does nothing when disposed.</remarks>
+		public static Scope Create(Action fnDispose)
+		{
+			return new Scope(fnDispose);
+		}
+
+		/// <summary>
+		/// Creates a <see cref="Scope" /> that disposes the specified object.
+		/// </summary>
+		/// <param name="disposable">The object to dispose.</param>
+		/// <returns>An instance of <see cref="Scope" /> that disposes the object when disposed.</returns>
+		/// <remarks>If disposable is null, the instance does nothing when disposed.</remarks>
+		public static Scope Create<T>(T disposable) where T : IDisposable
+		{
+			return disposable == null ? Empty : new Scope(disposable.Dispose);
+		}
+
+		/// <summary>
+		/// An empty scope, which does nothing when disposed.
+		/// </summary>
+		public static readonly Scope Empty = new Scope(null);
+
+		/// <summary>
+		/// Cancel the call to the encapsulated delegate.
+		/// </summary>
+		/// <remarks>After calling this method, disposing this instance does nothing.</remarks>
+		public void Cancel()
+		{
+			m_fnDispose = null;
+		}
+
+		/// <summary>
+		/// Returns a new Scope that will call the encapsulated delegate.
+		/// </summary>
+		/// <returns>A new Scope that will call the encapsulated delegate.</returns>
+		/// <remarks>After calling this method, disposing this instance does nothing.</remarks>
+		public Scope Transfer()
+		{
+			Scope scope = new Scope(m_fnDispose);
+			m_fnDispose = null;
+			return scope;
+		}
+
+		/// <summary>
+		/// Calls the encapsulated delegate.
+		/// </summary>
+		public void Dispose()
+		{
+			if (m_fnDispose != null)
+			{
+				m_fnDispose();
+				m_fnDispose = null;
+			}
+		}
+
+		private Scope(Action fnDispose)
+		{
+			m_fnDispose = fnDispose;
+		}
+
+		Action m_fnDispose;
+	}
+}
---
+++ tests/Logos.Utility.Tests/ScopeTests.cs
@@ -0,0 +1,58 @@
+﻿
+using NUnit.Framework;
+
+namespace Logos.Utility.Tests
+{
+    [TestFixture]
+    public class ScopeTests
+    {
+        [Test]
+        public void Empty()
+        {
+            // should compile and run without errors
+            using (Scope.Empty)
+            {
+            }
+        }
+
+        [Test]
+        public void Create()
+        {
+            int i = 0;
+            using (Scope.Create(() => i++))
+            {
+                Assert.That(i, Is.EqualTo(0));
+            }
+            Assert.That(i, Is.EqualTo(1));
+        }
+
+        [Test]
+        public void Cancel()
+        {
+            int i = 0;
+            using (Scope scope = Scope.Create(() => i++))
+            {
+                Assert.That(i, Is.EqualTo(0));
+                scope.Cancel();
+            }
+            Assert.That(i, Is.EqualTo(0));
+        }
+
+        [Test]
+        public void Transfer()
+        {
+            int i = 0;
+            using (Scope scope = Scope.Create(() => i++))
+            {
+                Assert.That(i, Is.EqualTo(0));
+
+                using (Scope transferred = scope.Transfer())
+                {
+                    Assert.That(i, Is.EqualTo(0));
+                }
+                Assert.That(i, Is.EqualTo(1));
+            }
+            Assert.That(i, Is.EqualTo(1));
+        }
+    }
+}
