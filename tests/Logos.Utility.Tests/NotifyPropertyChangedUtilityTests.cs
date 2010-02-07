94212b59acea51ca33665c033d9bedfb194d06f0
Bradley Grainger <bgrainger@gmail.com>
2010-02-07 00:49:27 -0800

    Added HasChanged. Closes #8.

---
+++ tests/Logos.Utility.Tests/NotifyPropertyChangedUtilityTests.cs
@@ -0,0 +1,20 @@
+﻿
+using System.ComponentModel;
+using Logos.Utility.ComponentModel;
+using NUnit.Framework;
+
+namespace Logos.Utility.Tests
+{
+	[TestFixture]
+	public class NotifyPropertyChangedUtilityTests
+	{
+		[TestCase("prop1", "prop1", true)]
+		[TestCase("prop1", "prop2", false)]
+		[TestCase(null, "prop1", true)]
+		public void HasChanged(string eventPropertyName, string testPropertyName, bool expected)
+		{
+			PropertyChangedEventArgs args = new PropertyChangedEventArgs(eventPropertyName);
+			Assert.That(args.HasChanged(testPropertyName), Is.EqualTo(expected));
+		}
+	}
+}
