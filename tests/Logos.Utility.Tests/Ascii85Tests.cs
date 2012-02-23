41c3a3cb55404ecbf31a95109ce17711c51e4926
Bradley Grainger <bgrainger@gmail.com>
2012-02-23 06:09:18 -0800

    Use constraint-based NUnit API.

--- tests/Logos.Utility.Tests/Ascii85Tests.cs
+++ tests/Logos.Utility.Tests/Ascii85Tests.cs
@@ -14,8 +14,8 @@ namespace Logos.Utility.Tests
 			const string c_encoded = @"9jqo^BlbD-BleB1DJ+*+F(f,q/0JhKF<GL>Cj@.4Gp$d7F!,L7@<6@)/0JDEF<G%<+EV:2F!,O<DJ+*.@<*K0@<6L(Df-\0Ec5e;DffZ(EZee.Bl.9pF""AGXBPCsi+DGm>@3BB/F*&OCAfu2/AKYi(DIb:@FD,*)+C]U=@3BN#EcYf8ATD3s@q?d$AftVqCh[NqF<G:8+EV:.+Cf>-FD5W8ARlolDIal(DId<j@<?3r@:F%a+D58'ATD4$Bl@l3De:,-DJs`8ARoFb/0JMK@qB4^F!,R<AKZ&-DfTqBG%G>uD.RTpAKYo'+CT/5+Cei#DII?(E,9)oF*2M7/c";
 			byte[] bytes = Encoding.ASCII.GetBytes(c_text);
 
-			Assert.AreEqual(c_encoded, Ascii85.Encode(bytes));
-			CollectionAssert.AreEqual(bytes, Ascii85.Decode(c_encoded));
+			Assert.That(Ascii85.Encode(bytes), Is.EqualTo(c_encoded));
+			Assert.That(Ascii85.Decode(c_encoded), Is.EqualTo(bytes));
 		}
 
 		[TestCase(new byte[0], "")]
