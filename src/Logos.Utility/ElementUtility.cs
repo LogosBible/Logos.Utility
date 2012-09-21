117638da7a1d5a8f27e27b68566cb93bd09e5934
Eric Brady <eric.brady@zoho.com>
2012-09-21 14:51:41 -0700

    Added HyperlinkUtility and MouseUtility to fulfill open issues #13 and #12, respectively.
    There is a third class added called ElementUtility which implements the
    functions referenced in HyperlinkUtility, which I assume Logos has
    somewhere not public, so I hope they don't use this public utility
    library and it conflicts.  If so, I can move the functions elsewhere.

---
+++ src/Logos.Utility/ElementUtility.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Windows;
+
+namespace Logos.Utility
+{
+	/// <summary>
+	/// Implementation of the ElementUtility.AddHandler and ElementUtility.RemoveHandler functions used in HyperlinkUtility.
+	/// </summary>
+	/// <remarks>See <a href="http://code.logos.com/blog/2008/01/hyperlinks_to_the_web_in_wpf.html">Hyperlinks to the Web in WPF</a>.</remarks>
+	public static class ElementUtility
+	{
+		/// <summary>
+		/// Adds a handler to the dependency object.
+		/// </summary>
+		/// <param name="d">The dependency object.</param>
+		/// <param name="re">The routed event.</param>
+		/// <param name="de">The handler delegate.</param>
+		public static void AddHandler(DependencyObject d, RoutedEvent re, Delegate de)
+		{
+			if (d is UIElement)
+			{
+				((UIElement)d).AddHandler(re, de);
+			}
+			else if (d is ContentElement)
+			{
+				((ContentElement)d).AddHandler(re, de);
+			}
+			else if (d is UIElement3D)
+			{
+				((UIElement3D)d).AddHandler(re, de);
+			}
+		}
+
+		/// <summary>
+		/// Removes a handler from the dependency object.
+		/// </summary>
+		/// <param name="d">The dependency object.</param>
+		/// <param name="re">The routed event.</param>
+		/// <param name="de">The handler delegate.</param>
+		public static void RemoveHandler(DependencyObject d, RoutedEvent re, Delegate de)
+		{
+			if (d is UIElement)
+			{
+				((UIElement)d).RemoveHandler(re, de);
+			}
+			else if (d is ContentElement)
+			{
+				((ContentElement)d).RemoveHandler(re, de);
+			}
+			else if (d is UIElement3D)
+			{
+				((UIElement3D)d).RemoveHandler(re, de);
+			}
+		}
+	}
+}
---
+++ src/Logos.Utility/MouseUtility.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows.Input;
+
+namespace Logos.Utility
+{
+	/// <summary>
+	/// Provides methods for mouse manipulation.
+	/// </summary>
+	/// <remarks> See <a href="http://code.logos.com/blog/2008/01/using_processstart_to_link_to.html">Using Process.Start to link to the Internet</a>.</remarks>
+	public static class MouseUtility
+	{
+		/// <summary>
+		/// Overrides the cursor with a busy one while a process is starting.
+		/// </summary>
+		/// <param name="process">The process (or webpage) to start.</param>
+		public static void OverrideCursor(String process)
+		{
+			try
+			{
+				Mouse.OverrideCursor = Cursors.AppStarting;
+				Process.Start(process);
+			}
+			catch (Exception)
+			{
+			}
+			finally
+			{
+				Mouse.OverrideCursor = null;
+			}
+		}
+	}
+}
