3f5c02a176f30b9820a439e7a967df5193e362c8
Bradley Grainger <bgrainger@gmail.com>
2013-02-25 22:30:31 -0800

    Add SourceIndex task to default build.

--- build.ps1
+++ build.ps1
@@ -1,4 +1,4 @@
-Task Default -depends Tests
+Task Default -depends SourceIndex
 
 Task Build {
   Exec { msbuild /m:4 /property:Configuration=Release Logos.Utility.sln }
@@ -7,4 +7,9 @@ Task Build {
 Task Tests -depends Build {
   mkdir build -force
   Exec { tools\NUnit\nunit-console.exe /nologo /framework=4.0 /xml=build\Logos.Utility.NUnit.xml tests\Logos.Utility.nunit }
+}
+
+Task SourceIndex -depends Tests {
+  $headSha = & "C:\Program Files (x86)\Git\bin\git.exe" rev-parse HEAD
+  Exec { tools\SourceIndex\github-sourceindexer.ps1 -symbolsFolder src\Logos.Utility\bin\Release -userId LogosBible -repository Logos.Utility -branch $headSha -sourcesRoot ${pwd} -dbgToolsPath "C:\Program Files (x86)\Windows Kits\8.0\Debuggers\x86" -gitHubUrl "https://raw.github.com" -serverIsRaw -verbose }
 }
