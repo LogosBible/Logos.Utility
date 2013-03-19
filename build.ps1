318c402804203c14beb3c1d60c26f616e4592c97
Bradley Grainger <bgrainger@gmail.com>
2013-03-19 10:41:56 -0700

    Add (non default) task to create NuGet package.

--- build.ps1
+++ build.ps1
@@ -13,3 +13,7 @@ Task SourceIndex -depends Tests {
   $headSha = & "C:\Program Files (x86)\Git\bin\git.exe" rev-parse HEAD
   Exec { tools\SourceIndex\github-sourceindexer.ps1 -symbolsFolder src\Logos.Utility\bin\Release -userId LogosBible -repository Logos.Utility -branch $headSha -sourcesRoot ${pwd} -dbgToolsPath "C:\Program Files (x86)\Windows Kits\8.0\Debuggers\x86" -gitHubUrl "https://raw.github.com" -serverIsRaw -verbose }
 }
+
+Task NuGetPack -depends SourceIndex {
+  Exec { nuget pack src\Logos.Utility\Logos.Utility.csproj -Prop Configuration=Release -Symbols }
+}
