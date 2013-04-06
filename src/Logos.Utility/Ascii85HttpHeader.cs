f865891eebadabf6949504254371b3ee81c826aa
Thomas Zetty <tzetty@gmail.com>
2013-04-06 15:33:07 -0500

    refactor Encode method to use pre-computed Plaintext => Ciphertext dictionaries. All test still pass.

--- src/Logos.Utility/Ascii85HttpHeader.cs
+++ src/Logos.Utility/Ascii85HttpHeader.cs
@@ -1,6 +1,7 @@
 ﻿using System;
-using System.Text;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
 
 namespace Logos.Utility
 {
@@ -29,7 +30,7 @@ namespace Logos.Utility
         /// </summary>
         /// <param name="ascii85">A valid Ascii85 string.</param>
         /// <returns>A string that is valid as an HTTP header.</returns>
-        public static string Encode(string ascii85)
+        public static string UNUSEDEncode(string ascii85)
         {
             if (ascii85 == null)
                 throw new ArgumentNullException("ascii85");
@@ -107,6 +108,53 @@ namespace Logos.Utility
         }
 
         /// <summary>
+        /// Encodes the specified Ascii85 string for safe passage in an HTTP header by
+        /// substituting other characters for characters used by Ascii85 in the set to
+        /// avoid the set of separator characters not allowed in HTTP headers, which is
+        /// {()&lt;&gt;@,;:\"/[]?=}. The curly braces aren't used by Ascii85, but cannot be
+        /// used in HTTP headers, either. Since there are 15 disallowed characters, and only
+        /// 6 printable ASCII characters that are both allowed in HTTP headers and not used
+        /// by Ascii85, one of those (~) is used as an escape character to introduce a
+        /// two-character set to represent each of 10 of those disallowed sequences.
+        /// 
+        /// This function would normally be used to encode the results of Ascii85.Encode().
+        ///  
+        /// </summary>
+        /// <param name="ascii85">A valid Ascii85 string.</param>
+        /// <returns>A string that is valid as an HTTP header.</returns>
+        public static string Encode(string ascii85)
+        {
+            if (ascii85 == null)
+                throw new ArgumentNullException("ascii85");
+
+            // We will pre-compute Plaintext to Ciphertext dictionaries based on the CTtoPT ones
+            // To allow this to occur only once, we lock
+            if (PTtoCT == null)
+                CreatePTtoCT();
+            Debug.Assert(PTtoCT != null);
+
+            // Instantiate a StringBuilder with more than enough room to store the new string.
+            StringBuilder sb = new StringBuilder(ascii85.Length * 2);
+
+            // walk the characters
+            foreach (char ch in ascii85)
+            {
+                if (PTtoCT.ContainsKey(ch))
+                {
+                    sb.Append(PTtoCT[ch]);
+                }
+                else
+                {
+                    if ((ch < Ascii85.FirstChar) || (ch > Ascii85.LastChar))
+                        throw new FormatException("Invalid character '{0}' in Ascii85 string.".FormatInvariant(ch));
+                    else
+                        sb.Append(ch);
+                }
+            }
+            return sb.ToString();
+        }
+
+        /// <summary>
         /// Decodes the specified HTTP parameter string to an Ascii85 string.
         /// The results of this function would normally be fed to Ascii85.Decode().
         /// </summary>
@@ -177,11 +225,30 @@ namespace Logos.Utility
             { 'j', '=' },
         };
 
-        static IDictionary<char, string> getPTtoCT()
-        {
-            var result = new Dictionary<char, string>();
+        // these are only used by the Encode method
+        static object encoderLock = new object();
+        static IDictionary<char, string> PTtoCT = null;
 
-            return result;
+        static void CreatePTtoCT()
+        {
+            // Build Plaintext to Ciphertext dictionaries based on the CTtoPT ones
+            // To allow this to occur only once, we lock
+            lock (encoderLock)
+            {
+                PTtoCT = new Dictionary<char, string>();
+
+                foreach (KeyValuePair<char, char> entry in CTtoPT)
+                {
+                    PTtoCT.Add(entry.Value, entry.Key.ToString());
+                }
+                // in addition, pass z through unchanged.
+                PTtoCT.Add('z', "z");
+
+                foreach (KeyValuePair<char, char> entry in tildeCTtoPT)
+                {
+                    PTtoCT.Add(entry.Value, Tilde + entry.Key.ToString());
+                }
+            }
         }
     }
 }
