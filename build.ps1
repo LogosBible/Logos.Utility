dc784e6da62667ed6104387b4a3633fd4637f4f0
Bradley Grainger <bgrainger@gmail.com>
2013-05-13 22:44:02 -0700

    Add Coverity source analysis.

--- build.ps1
+++ build.ps1
@@ -1,4 +1,4 @@
-Task Default -depends SourceIndex
+Task Default -depends Coverity, SourceIndex
 
 Task Build {
   Exec { msbuild /m:4 /property:Configuration=Release Logos.Utility.sln }
@@ -7,6 +7,19 @@ Task Build {
 Task Tests -depends Build {
   mkdir build -force
   Exec { tools\NUnit\nunit-console.exe /nologo /framework=4.0 /xml=build\Logos.Utility.NUnit.xml tests\Logos.Utility.nunit }
+}
+
+Task Coverity -depends Tests {
+  mkdir build\coverity -force
+  & "C:\Program Files\Coverity\Coverity Static Analysis\bin\cov-analyze-cs.exe" `--all `--max-mem 8192 `--dir build\coverity src\Logos.Utility\bin\Release\Logos.Utility.dll
+  if ($LastExitCode -ne 0) {
+    throw "Error executing cov-analyze-cs.exe"
+  }
+  $headSha = & "C:\Program Files (x86)\Git\bin\git.exe" rev-parse HEAD
+  & "C:\Program Files\Coverity\Coverity Static Analysis\bin\cov-commit-defects.exe" `--host coverity `--dataport 9090 `--stream Logos.Utility `--user logosutilityreporter `--password password `--dir build\coverity `--version $headSha `--strip-path $(get-location).Path
+  if ($LastExitCode -ne 0) {
+    throw "Error executing cov-commit-defects.exe"
+  }
 }
 
 Task SourceIndex -depends Tests {
